bfcc492836cfd4af80e348ed061368813554fbce
Dinesh Rout <rddinesh779@gmail.com>
2022-03-09 14:44:46 +0530

    refactor

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,29 +2,27 @@ import { Container, Nav, Navbar } from 'react-bootstrap';
 
 const Header = () => {
   return (
-    <header>
-      <Navbar
-        collapseOnSelect
-        expand='lg'
-        className='navbar'
-        variant='dark'
-        sticky='top'
-      >
-        <Container>
-          <Navbar.Brand href='#home'>Logo</Navbar.Brand>
-          <Navbar.Toggle aria-controls='responsive-navbar-nav' />
-          <Navbar.Collapse id='responsive-navbar-nav'>
-            <Nav className='me-auto'>
-              <Nav.Link href='/latest'>Latest</Nav.Link>
-              <Nav.Link href='/movies'>Movies</Nav.Link>
-              <Nav.Link href='/news'>News</Nav.Link>
-              <Nav.Link href='/about'>about</Nav.Link>
-              <Nav.Link href='/contact'>contact</Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-    </header>
+    <Navbar
+      collapseOnSelect
+      expand='lg'
+      className='navbar'
+      variant='dark'
+      sticky='top'
+    >
+      <Container>
+        <Navbar.Brand href='#home'>Logo</Navbar.Brand>
+        <Navbar.Toggle aria-controls='responsive-navbar-nav' />
+        <Navbar.Collapse id='responsive-navbar-nav'>
+          <Nav className='me-auto'>
+            <Nav.Link href='/latest'>Latest</Nav.Link>
+            <Nav.Link href='/movies'>Movies</Nav.Link>
+            <Nav.Link href='/news'>News</Nav.Link>
+            <Nav.Link href='/about'>about</Nav.Link>
+            <Nav.Link href='/contact'>contact</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 };
 
