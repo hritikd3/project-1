7ced2079fdc48cc5d800f13cbcd5fe294d4ce1bd
Dinesh Rout <rddinesh779@gmail.com>
2022-03-19 10:50:14 +0530

    bug fix

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import { useLayoutEffect } from 'react';
 import { ThemeProvider } from 'react-bootstrap';
+import { useLocation } from 'react-router';
 import { Outlet } from 'react-router';
 import Footer from './components/Footer';
 import Header from './components/Header';
 
 const App = () => {
+  const location = useLocation();
+
+  useLayoutEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location.pathname]);
+
   return (
     <ThemeProvider
       breakpoints={['xxxl', 'xxl', 'xl', 'lg', 'md', 'sm', 'xs', 'xxs']}
